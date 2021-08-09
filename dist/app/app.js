9f9375952b451fc188fe419cdbc674f9a7b60686
Loren Dujakovic <loren.dujakovic@mail.ch>
2021-08-09 14:02:47 +0200

    add fixes to site

--- dist/app/app.js
+++ dist/app/app.js
@@ -4,7 +4,7 @@ const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const menu = document.querySelector('.header__menu');
 const fadeElems = document.querySelectorAll('.has-fade');
-
+//Mobile Menu Open & Close
 btnHamburger.addEventListener('click', () => {
 
     if (header.classList.contains('open')) {
@@ -21,5 +21,27 @@ btnHamburger.addEventListener('click', () => {
             element.classList.remove('fade-out');
             element.classList.add('fade-in'); 
         });
+    }
+});
+
+
+//Variables Elements for Costumer Flex Container
+const item = document.querySelector('.costumers__flex');
+// Heart Animation Costumers
+item.addEventListener('click', event => {
+    if(event.target && event.target.matches('.costumers__item-like')) {
+        event.target.classList.toggle('active');   
+    }
+});
+
+
+// Navmenu Background-color change
+window.addEventListener('scroll', function() {
+    let offset = window.pageYOffset;
+
+    if (offset > 75) {
+        header.classList.add('scroll')
+    }else{
+        header.classList.remove('scroll')
     }
 });
